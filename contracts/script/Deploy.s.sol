96bc83e6d03e08f0cdd062b6216265c2812f43fe
Paterson <86971279+paterson1@users.noreply.github.com>
2024-06-14 16:00:16 -0300

    feat: deploy contract to anvil

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -2,8 +2,19 @@
 pragma solidity 0.8.4;
 
 import { BaseScript } from "./Base.s.sol";
+import "../src/AnonVoting.sol";
+import "../node_modules/semaphore/packages/contracts/contracts/base/SemaphoreVerifier.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast { }
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        SemaphoreVerifier verifier = new SemaphoreVerifier();
+
+        AnonVoting anonVoting = new AnonVoting(verifier);
+
+        vm.stopBroadcast();
+    }
 }
