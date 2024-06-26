649d8d433bfe35d228cdab69fe5bf71cd26ac7dd
Tomás Rojas <tmsrjs@gmail.com>
2024-06-26 13:27:08 -0300

    style: keep linter happy

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.4;
 
 import { BaseScript } from "./Base.s.sol";
-import { AnonVoting } from  "src/AnonVoting.sol";
+import { AnonVoting } from "src/AnonVoting.sol";
 import { SemaphoreVerifier } from "semaphore/base/SemaphoreVerifier.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
